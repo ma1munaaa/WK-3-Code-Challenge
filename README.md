6ff7d163872fe241e33f9865fc0e53f282305e5e
maimuna <mohamudmaimuna003@gmail.com>
2023-07-06 21:19:09 +0300

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# WK-3-Code-Challenge
+# WK-3-Code-challenge
+# Movie Ticket Booking Project
+
+This is a simple movie ticket booking project that allows users to view a list of movies and their details, such as title, runtime, description, showtime, and available seats. The project is implemented using HTML, CSS, and JavaScript.
+
+## How to Use
+
+1. Clone or download this repository to your local machine.
+2. Open the `index.html` file in a web browser.
+3. The list of movies will be displayed with their respective details.
+4. Scroll through the list to view the available movies.
+5. The movie details include the movie title, runtime, description, showtime, and the number of available seats.
+6. Select the movie you want to watch and check the available seats.
+7. Book your ticket based on the available seats shown.
+8. Enjoy the movie!
+
+The movie data is stored in the `db.json` file. Each movie has properties such as `id`, `title`, `runtime`, `description`, `showtime`, `capacity`, and `tickets_sold`. The `poster` property contains the URL to the movie poster image.
+
+## Credits
+
+This project was created by Maimuna. It utilizes the following technologies:
+
+- HTML
+- CSS
+- JavaScript
+- JSON
---
+++ index.js
@@ -0,0 +1,56 @@
+function fetchFilms() {
+    fetch('https://api.npoint.io/0b2aef194151f5771a43/films/')
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Unable to fetch film data');
+        }
+      })
+      .then(data => renderFilms(data))
+      .catch(error => {
+        console.log(error);
+        renderErrorMessage();
+      });
+  }
+  
+  function renderFilms(data) {
+    const div = document.getElementById('card');
+    const ul = document.getElementById('films');
+  
+    data.forEach(movie => {
+      const li = document.createElement('li');
+      li.classList.add('pointer', 'bold-italic-text');
+      li.innerHTML = movie.title;
+  
+      const filmCard = document.createElement("div");
+      filmCard.classList.add('film-card');
+      filmCard.innerHTML = `
+        <img src="${movie.poster}" height="500px" width="300px"/>
+        <h2 class="bold-text">${movie.title}</h2>
+        <p class="bold-text">${movie.description}</p>
+        <p><span class="highlight bold-text">Runtime: ${movie.runtime}</span></p>
+        <p><span class="highlight bold-text">Showtime: ${movie.showtime}</span></p>
+        <p class="bold-italic-text">Available tickets: ${(movie.capacity) - (movie.tickets_sold)}</p>
+        <button class="buy-button">Buy ticket</button>
+      `;
+  
+      li.addEventListener('click', () => {
+        div.innerHTML = "";
+        div.appendChild(filmCard);
+        if (!filmCard.classList.contains('active')) {
+          filmCard.classList.add('active');
+          div.appendChild(filmCard);
+        }
+      });
+  
+      ul.appendChild(li);
+    });
+  }
+  
+  function renderErrorMessage() {
+    const div = document.getElementById('card');
+    div.innerText = "Unable to fetch film data. Please try again later.";
+  }
+  
+  fetchFilms();
